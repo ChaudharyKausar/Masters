a383206c5bb90349e9aa3d0e7645f9fae344a627
Kausar Choudhary <ch.kausar2005@gmail.com>
2025-06-19 14:41:12 +0530

    replace the blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,14 +49,23 @@
   /**
    * Toggle mobile nav dropdowns
    */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
+  document.querySelectorAll('.navmenu li.dropdown > a').forEach(item => {
+  item.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Toggle active class on parent <li>
+    this.parentNode.classList.toggle('active');
+
+    // Toggle the next sibling <ul> dropdown menu
+    const dropdownMenu = this.nextElementSibling;
+    if (dropdownMenu && dropdownMenu.tagName === 'UL') {
+      dropdownMenu.classList.toggle('dropdown-active');
+    }
+
+    e.stopImmediatePropagation();
   });
+});
+
 
 
   // document.addEventListener('DOMContentLoaded', () => {
